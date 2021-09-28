8df292fb052a6526bde15607814a772b643c7d16
cristian <cra.bustamante@alumnos.duoc.cl>
2021-09-28 13:53:53 -0300

    add morgan and middleware

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const morgan = require('morgan');
 const libro = require('./routes/libro');
 
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
+
+//uso de middleware (morgan) en modo desarrollo
+if (process.env.NODE_ENV === 'development') {
+    
+    app.use(morgan('dev'));
+}
+
 
 app.use('/api/libro', libro);
 
