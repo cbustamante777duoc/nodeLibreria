03b4b02ed199335793c17cbb4c7ef15280162b4b
cristian <cra.bustamante@alumnos.duoc.cl>
2021-09-28 11:46:37 -0300

    created the methods get, post, put and delete of the api

--- server.js
+++ server.js
@@ -4,6 +4,53 @@ const express = require('express');
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
+
+/**
+ * metodo que devuelve un respuesta 200 al momento consultar la api
+ */
+app.get('/api/Libro',(req,res) => {
+  res.status(200).json(
+      {status:200, mensaje:'Se proceso correctamente'});
+
+});
+
+/**
+ * metodo que devuelve un respuesta 200 al momento consultar la api POR ID
+ */
+app.get('/api/Libro/:id',(req,res) => {
+    res.status(200).json(
+        {status:200, mensaje:'Se devuelve el libro por id'});
+  
+  });
+
+/**
+ * metodo que crea un libro por el metodo post
+ */
+
+  app.post('/api/Libro',(req,res) => {
+    res.status(200).json(
+        {status:200, mensaje:'Se ha creado el libro correctamente'});
+  
+  });
+/**
+ * metodo que modifica un libro por el metodo put mas la ID
+ */
+  app.put('/api/Libro/:id',(req,res) => {
+    res.status(200).json(
+        {status:200, mensaje:'Se ha modificado el libro correctamente'});
+  
+  });
+
+  /**
+ * metodo que elimina un libro por el metodo delete mas la ID
+ */
+   app.delete('/api/Libro/:id',(req,res) => {
+    res.status(200).json(
+        {status:200, mensaje:'Se ha eliminado el libro correctamente'});
+  
+  });
+
+
 //encaso que el valor sea indefinido utiliza el 5000
 const PORT = process.env.PORT || 5000;
 
