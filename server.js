b950ea0c0fb85acf91a860683f11755ad71d2357
cristian <cra.bustamante@alumnos.duoc.cl>
2021-09-28 19:36:43 -0300

    conexion existosa por el momento

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const morgan = require('morgan');
-const libro = require('./routes/libro');
+const dotenv = require("dotenv");
+const express = require("express");
+const morgan = require("morgan");
+const connectDatabase = require("./config/db");
 
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
+
+connectDatabase();
+
+const libro = require("./routes/libro");
 
 const app = express();
 
 //uso de middleware (morgan) en modo desarrollo
-if (process.env.NODE_ENV === 'development') {
-    
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-
-app.use('/api/libro', libro);
-
-
+app.use("/api/libro", libro);
 
 //encaso que el valor sea indefinido utiliza el 5000
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log('servidor se ejecuta en ambiente', process.env.NODE_ENV));
+const server = app.listen(
+  PORT,
+  console.log("servidor se ejecuta en ambiente", process.env.NODE_ENV)
+);
+
+process.on('unhandledRejection',(err, promise) =>{
+    console.log('Errores', err.message);
+    server.close(() => process.exit(1));
+
+})
